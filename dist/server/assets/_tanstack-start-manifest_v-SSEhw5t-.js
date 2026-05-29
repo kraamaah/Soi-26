f430eea8e0441300ede69a2ae8962bc083c00418
kraamaah <ananttripathi225@gmail.com>
2026-05-29 07:48:18 +0530

    yay soi website

--- dist/server/assets/_tanstack-start-manifest_v-SSEhw5t-.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Anant/Desktop/SoI'26/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-BNCDipcY.js"] }, "/": { filePath: "C:/Users/Anant/Desktop/SoI'26/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-bG6c8-DR.js"] } }, clientEntry: "/assets/index-BNCDipcY.js" });
-export {
-  tsrStartManifest
-};
